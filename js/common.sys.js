64d3be0413ed3a1e236d0c3a753a80f5fd8d73b8
tang-love-1314 <tscjobs@163.com>
2019-03-29 15:17:47 +0800

    add from new slide-menu replace bootstarap veritical-menu

---
+++ js/common.sys.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2019/03/29.
+ */
+$(function () {
+    'use strict'
+
+    $('[data-toggle="sys-slide-toggle"]').on('click', function () {
+        $('.slide-toggle').toggleClass('open')
+    })
+})
